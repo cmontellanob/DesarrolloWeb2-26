79a7250ea52d256bff152b367215b8e8670c6c52
cmontellanob <carlosmontellano@gmail.com>
2026-08-19 17:41:27 -0400

    19-08

--- Tema 3 php/08-19 arreglos funciones form/login.php
+++ Tema 3 php/08-19 arreglos funciones form/login.php
@@ -8,3 +8,12 @@
   } else {
       echo "Credenciales incorrectas";
   }
+
+  ?>
+<h1>variable server</h1>
+<?php 
+foreach($_SERVER as $clave=>$valor )
+    {
+        echo " clave: $clave => $valor <br>";
+    }
+?>
